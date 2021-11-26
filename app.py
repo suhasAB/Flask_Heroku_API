1e9e53a51bfcee80f5a228ba493dab1042a90b4d
Suhas Anand Balagar <suhasbalagar@gmail.com>
2021-11-25 20:56:36 -0800

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,47 @@ from flask import request
 import math
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+
+
+# app.py
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+@app.route('/getmsg/', methods=['GET'])
+def respond():
+    # Retrieve the name from url parameter
+    name = request.args.get("name", None)
+
+    # For debugging
+    print(f"got name {name}")
+
+    response = {}
+
+    # Check if user sent a name at all
+    if not name:
+        response["ERROR"] = "no name found, please send a name."
+    # Check if the user entered a number not a name
+    elif str(name).isdigit():
+        response["ERROR"] = "name can't be numeric."
+    # Now the user entered a valid name
+    else:
+        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
+
+    # Return the response in json format
+    return jsonify(response)
+
+
+
+# A welcome message to test our server
+@app.route('/')
+def index():
+    return "<h1>Welcome to our server !!</h1>"
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
+
+
 
 #create a flask route and method to verify if a given number is prime and even
 @app.route('/is_prime/<int:number>')
