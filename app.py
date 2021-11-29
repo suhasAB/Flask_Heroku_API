dca1a59f22f1757dff078ca9abd8c67e8e42b49a
Suhas Anand Balagar <suhasbalagar@gmail.com>
2021-11-28 20:13:52 -0800

    removing unwanted routes

--- app.py
+++ app.py
@@ -10,31 +10,6 @@ app = Flask(__name__)
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 
-@app.route('/getmsg/', methods=['GET'])
-def respond():
-    # Retrieve the name from url parameter
-    name = request.args.get("name", None)
-
-    # For debugging
-    print(f"got name {name}")
-
-    response = {}
-
-    # Check if user sent a name at all
-    if not name:
-        response["ERROR"] = "no name found, please send a name."
-    # Check if the user entered a number not a name
-    elif str(name).isdigit():
-        response["ERROR"] = "name can't be numeric."
-    # Now the user entered a valid name
-    else:
-        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
-
-    # Return the response in json format
-    return jsonify(response)
-
-
-
 # A welcome message to test our server
 @app.route('/')
 def index():
