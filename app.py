8651c7b8d2dc76d6a70c5028228fcc67be87ddb6
Suhas Anand Balagar <suhasbalagar@gmail.com>
2021-11-28 20:14:36 -0800

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 # A welcome message to test our server
 @app.route('/')
 def index():
-    return "<h1>Welcome to our server !!</h1>"
+    return "Welcome to our server !!"
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
