1f25cdc23c93e4e2b18e0ddf734a0f30892441d7
Suhas Anand Balagar <suhasbalagar@gmail.com>
2021-11-29 21:48:47 -0800

    adding new Flask Route by user

--- trial.py
+++ trial.py
@@ -5,3 +5,8 @@ def is_valid_email(email):
 def is_valid_email(email):
     return re.match(r"[^@]+@[^@]+\.[^@]+", email) != None
 
+
+@app.route('/is_valid_email/<email>')
+def is_valid_email(email):
+    return re.match(r"[^@]+@[^@]+\.[^@]+", email) != None
+
