d0f55929bd2e03adf2783b9fe242b82530371a56
CodelessAppDeveloper <95279988+CodelessAppDeveloper@users.noreply.github.com>
2021-11-30 02:49:56 -0800

    added new FlaskRoute by @suhasAB which was commited by @CodelessAppDev

--- app.py
+++ app.py
@@ -99,3 +99,8 @@ def is_prime(number):
     else:
         return str(number) + " is not a prime number"
 
+
+@app.route('/is-even/<int:number>')
+def is_even(number):
+    return f'{number} is {"even" if number % 2 == 0 else "odd"}'
+
