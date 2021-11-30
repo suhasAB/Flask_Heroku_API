34b742d8cec79930320effba1b8574882d293d1c
Suhas Anand Balagar <suhasbalagar@gmail.com>
2021-11-30 01:03:59 -0800

    adding new Flask Route by user:suhasAB

--- app.py
+++ app.py
@@ -95,3 +95,8 @@ def isZ(string):
     else:
         return 'False'
 
+
+@app.route('/is-even/<int:number>')
+def is_even(number):
+    return f'{number} is {"even" if number % 2 == 0 else "odd"}'
+
